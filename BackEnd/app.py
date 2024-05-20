705bae82b858d59acc2bfe9bd1924c1bd1b68e5d
Yuchen_Zhou <645251181@qq.com>
2024-05-20 22:31:34 +0800

    0.0.6 修改了部分配置内容

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,10 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
-from BackEnd.Env.handle import configs
+from Env.handle import configs
 
 app = FastAPI()
 
-print(configs.get_params())
 
+@app.get('/hello')
+def hello():
+    return {'mess': 'hello'}
+
+
+print(configs.get_params())
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run(app, host='0.0.0.0', port=int(configs.BACKEND_PORT))
