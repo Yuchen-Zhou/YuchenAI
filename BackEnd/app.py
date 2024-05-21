6bef5059a8a00cf1ef62dd7266b1dac86ef9e0a3
Yuchen_Zhou <645251181@qq.com>
2024-05-21 09:46:24 +0800

    0.0.7 配置了跨域

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,9 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Env.handle import configs
 
 app = FastAPI()
 
+origins = [
+    "http://192.168.124.4:3000",
+    # 你可以添加其他允许的源
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/hello')
 def hello():
--- FrontEnd/pages/index.vue
+++ FrontEnd/pages/index.vue
@@ -1,10 +1,29 @@
 <template>
-    <div class="common-layout">
-        <el-header>
-          <Topbar />
-        </el-header>
-        <el-main>
-          <carousel />
-        </el-main>
-    </div>
+  <div class="common-layout">
+    <el-header>
+      <Topbar />
+    </el-header>
+    <el-main>
+      <carousel />
+    </el-main>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+const response = ref(null);
+
+onMounted(async () => {
+  try {
+    const res = await fetch('http://192.168.124.4:8000/hello', { method: 'GET' });
+    if (res.ok) {
+      response.value = await res.json();
+      console.log(response.value);
+    } else {
+      console.error('Fetch error: ', res.status);
+    }
+  } catch (error) {
+    console.error('Fetch error: ', error);
+  }
+});
+</script>
